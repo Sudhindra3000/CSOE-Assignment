6d9f39b7b6ebbc037484af8716a5365c469e1e2d
Sudhindra3000 <55095527+Sudhindra3000@users.noreply.github.com>
2022-03-30 16:15:53 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+/// Expands the input when clicked on label
+function expand(label) {
+    var elemId = label.getAttribute("for");
+    document.getElementById(elemId).style.height = "45px";
+    document.getElementById(elemId).classList.add("my-style");
+    label.style.transform = "translateY(-45px)";
+}
